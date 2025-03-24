f72dc02fe1a195fbef8a1d0764d7efab3b4b27a4
lxy1012 <272432916@qq.com>
2025-03-24 19:29:34 +0800

    mini mode:Based on a finite state machine, the interaction between the character and the scene is realized - touching the objects of the scene

--- TestDemo_PackageBuilder/Assets/Editor/PackagePlatformSettings/EditorConstParm.cs
+++
@@ -1,5 +0,0 @@
-public class EditorConstParm
-{
-    public const string ASSET_BUNDLE_GLOBAL_SETTINGS = "Assets/Editor/GlobalConfig/PackagePlatformSettings.asset";
-
-}
--- TestDemo_PackageBuilder/ProjXlsx2Protobuf/1xlsx2proto_binaryproto.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-::echo [INFO] current dir:%curdir%
-cd /d %~dp0
-cd src
-::需配置python环境变量
-python ./generate.py
-
-exit
